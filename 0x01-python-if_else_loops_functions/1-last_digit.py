5f20a5362351491c7e3f80c2e1f7d28486437177
Omoseke Salami <omosekesalami@gmail.com>
2022-11-29 04:51:22 -0800

    added negative possibility

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-lastDigit = number % 10
+if number > 0:
+    lastDigit = number % 10
+else:
+     lastDigit = (-number % 10) * -1
 if lastDigit > 5:
     print(f'Last digit of {number} is {lastDigit} and is greater than 5')
 elif lastDigit == 0:
