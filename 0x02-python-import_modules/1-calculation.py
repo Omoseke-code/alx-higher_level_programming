95d08325c2452bde1fb7bc312b10ded5b4a6d014
Omoseke Salami <omosekesalami@gmail.com>
2022-12-01 03:52:54 -0800

    task2

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-"""Does some basic calculations"""
+
 if __name__ == "__main__":
-    import calculator_1
+    """Print the sum, difference, multiple and quotient of 10 and 5."""
+    from calculator_1 import add, sub, mul, div
+
     a = 10
     b = 5
-    print(f"{a} + {b} = ", calculator_1.add(a, b))
-    print(f"{a} - {b} = ", calculator_1.sub(a, b))
-    print(f"{a} * {b} = ", calculator_1.mul(a, b))
-    print(f"{a} / {b} = ", calculator_1.div(a, b))
+
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
