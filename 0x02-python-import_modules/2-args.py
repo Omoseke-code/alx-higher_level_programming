8bc9438d6baa8883331c14a273ef41fd82c18b5c
Omoseke Salami <omosekesalami@gmail.com>
2022-12-01 03:57:01 -0800

    task3

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
     elif count == 1:
         print("1 argument:")
     else:
-        print(f"{count} arguments:")
+        print("{} arguments:".format(count))
     for i in range(count):
-        print(f"{i + 1}: ", sys.argv[i + 1])
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
