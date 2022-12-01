b1b74598cd08579bcfcc59ceb59384ecb4ccb750
Omoseke Salami <omosekesalami@gmail.com>
2022-12-01 01:51:28 -0800

    a program that imports the variable a from the file variable_load_5.py and prints its value

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""imports the variable a from the file variable_load_5.py\
+        and prints its value"""
+if __name__ == "__main__":
+    import variable_load_5
+    print(variable_load_5.a)
