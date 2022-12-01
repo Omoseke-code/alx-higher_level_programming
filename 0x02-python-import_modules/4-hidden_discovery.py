d621c1ce98d3ffb3890adb4a22afc38d50f59b4c
Omoseke Salami <omosekesalami@gmail.com>
2022-12-01 01:45:24 -0800

    program that prints all the names defined by the compiled module hidden_4.pyc

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""prints all the names defined by the compiled module"""
+if __name__ == "__main__":
+    import hidden_4
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
