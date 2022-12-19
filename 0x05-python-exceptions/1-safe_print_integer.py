f9b6d4d4481f909c0deafca00304a486279cbfd5
Omoseke Salami <omosekesalami@gmail.com>
2022-12-19 06:36:15 -0800

    new

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -5,5 +5,4 @@ def safe_print_integer(value):
         print("{:d)".format(value))
         return (True)
     except (TypeError, ValueError):
-        pass
-    return (False)
+        return (False)
