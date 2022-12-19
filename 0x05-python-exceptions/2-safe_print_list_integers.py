61434cbca196fc06a7007b11f63bca3101342efd
Omoseke Salami <omosekesalami@gmail.com>
2022-12-19 07:21:48 -0800

    2-safe_print_list_integers.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -8,5 +8,5 @@ def safe_print_list_integers(my_list=[], x=0):
             counter += 1
         except (TypeError, ValueError):
             continue
-        print()
-        return counter
+    print()
+    return counter
