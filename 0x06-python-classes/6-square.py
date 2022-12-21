7424dda3ca4f2b6a99ccba2949f35f1c090d078b
Omoseke Salami <omosekesalami@gmail.com>
2022-12-21 09:19:50 -0800

    square with position spaces

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -59,10 +59,12 @@ class Square:
 
     def my_print(self):
         """prints the square"""
-        if self.size != 0:
-            for square_p in range(0, self.__position[1]):
-                print()
-            for i in range(0, self.size):
-                print('{}{}'.format(' ' * self.__position[0], '#'*self.__size))
-        else:
-            print()
+        if self.__size == 0:
+            print("")
+            return
+
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            print("")
