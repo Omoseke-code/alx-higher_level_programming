6982dbd1c16e9a7d1ee57406e2cd53584606eb66
Omoseke Salami <omosekesalami@gmail.com>
2023-01-04 13:59:40 -0800

    fixed pycodestyle issue

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -6,6 +6,7 @@ class Rectangle:
     """defining a rectangle"""
 
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         """initalisation
         Args:
@@ -85,5 +86,3 @@ class Rectangle:
 
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
-
-
