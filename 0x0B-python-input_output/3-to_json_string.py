e2c8a07f287ddb4a897cc0b19a48faab09ad7a8a
Omoseke Salami <omosekesalami@gmail.com>
2023-01-12 12:44:46 -0800

    3-to_json_string.py

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import json
+"""3-to_json_string"""
+
+
+def to_json_string(my_obj):
+    """a function that returns the JSON representation
+    of an object (string)"""
+    return json.dumps(my_obj)
