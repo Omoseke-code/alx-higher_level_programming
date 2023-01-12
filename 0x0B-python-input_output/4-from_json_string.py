29859088b7ca963c062b3b9846781dc5169f083b
Omoseke Salami <omosekesalami@gmail.com>
2023-01-12 13:04:09 -0800

    a function that returns an object (Python data structure) represented by a JSON string

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""4-from_json_string"""
+import json
+
+
+def from_json_string(my_str):
+    """a function that returns an object (Python data structure)
+    represented by a JSON string"""
+    return json.loads(my_str)
